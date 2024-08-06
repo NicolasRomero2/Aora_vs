edce75f139d0c59e378b5a67c58c1252a551595d
Aprendiz <Aprendiz@BOGDFPCGMF1049.FormacionSena.RED>
2024-08-06 14:35:50 -0500

    Cambio en el index

--- app/index.tsx
+++ app/index.tsx
@@ -7,11 +7,10 @@ export default function App(){
     
     <View className="flex-1 items-center justify-center
     bg-white" >
-    <Text className="text-3xl">Aora Adso!</Text>
+    <Text className="text-3xl">Aora Adso! :D</Text>
     <StatusBar style='auto' />
     <Link href="/profile" style={{ color: 'blue'}}>Go to Profile</Link>
     </View>
 
   )
 }
-
