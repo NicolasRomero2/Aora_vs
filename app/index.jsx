93137602b59069b235b8300d8a48ca7b447c30d4
Aprendiz <Aprendiz@BOGDFPCGMF1049.FormacionSena.RED>
2024-08-13 16:11:28 -0500

    Actualizacion de botones inferiores de navegación

--- app/index.jsx
+++ app/index.jsx
@@ -1,16 +1,16 @@
 import { Text, View } from "react-native";
 import { StatusBar } from "expo-status-bar";
 import { Link } from "expo-router";
-import React from "react";
+
 
 export default function App() {
   return (
 
     <View className="flex-1 items-center justify-center
     bg-white" >
-      <Text className="text-3xl">Aora Adso! :D</Text>
+      <Text className="text-3xl font-pblack">Aora Adso! :D</Text>
       <StatusBar style='auto' />
-      <Link href="/profile" style={{ color: 'blue' }}>Go to Profile</Link>
+      <Link href="/home" style={{ color: 'blue' }}>Go to Home</Link>
     </View>
   )
 }
